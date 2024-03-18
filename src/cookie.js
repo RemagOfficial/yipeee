6bda91c4581132385f789e08d7ebb1c30176557f
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-03-18 21:37:13 +0100

    cookie.js file reformat

--- src/cookie.js
+++ src/cookie.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     console.debug("Cookie clicker init");
     let AutoClickerCost = 20;
     let AutoClickerClickerCost = 20;
@@ -30,7 +30,9 @@
 
     function getCookie(name) {
         let nameEQ = name + "=";
-        let ca = document.cookie.split(';');
+        let cs = document.cookie;
+        if (cs == 0) return null;
+        let ca = cs.split(';');
         for (let i = 0; i < ca.length; i++) {
             let c = ca[i];
             while (c.charAt(0) == ' ') c = c.substring(1, c.length);
@@ -40,20 +42,24 @@
     }
 
     function clearCookies() {
-      let browserCookies = document.cookie.split(';');
-      console.log(browserCookies);
+        let browserCookies = document.cookie.split(';');
 
-      // set 1 Jan, 1970 expiry for every cookies
-      for (let i = 0; i < browserCookies.length; i++)
-      document.cookie = browserCookies[i] + "=;expires=" + new Date(0).toUTCString();
-      console.log(browserCookies);
+        console.log(browserCookies);
+
+        // set 1 Jan, 1970 expiry for every cookies
+        for (let i = 0; i < browserCookies.length; i++)
+            document.cookie = browserCookies[i] + "v= ;expires=" + new Date(0).toUTCString();
+        delete document.cookie
+        console.log(browserCookies);
     }
 
     function receiveCookie(amount) {
         cookies += amount;
         let clicks = document.getElementById("clicks");
         clicks.className = "show";
-        setTimeout(function(){ clicks.className = clicks.className.replace("show", ""); }, 3000);
+        setTimeout(function () {
+            clicks.className = clicks.className.replace("show", "");
+        }, 3000);
     }
 
     function receiveCookieAuto(amount) {
@@ -65,7 +71,8 @@
     }
 
     function toggleShopDisplay() {
-        if (shopShown) {            AutoClickers > 0;
+        if (shopShown) {
+            AutoClickers > 0;
             document.getElementById("shopMenuDiv").style.display = "none";
             shopShown = false;
         } else {
@@ -76,7 +83,8 @@
     }
 
     function toggleUpgradeDisplay() {
-        if (shopShown) {            AutoClickers > 0;
+        if (shopShown) {
+            AutoClickers > 0;
             document.getElementById("upgradeMenuDiv").style.display = "none";
             shopShown = false;
         } else {
@@ -93,73 +101,75 @@
         toaster.style.borderColor = borderColor;
         toaster.className = "show";
 
-        setTimeout(function(){ toaster.className = toaster.className.replace("show", ""); }, 3000)
+        setTimeout(function () {
+            toaster.className = toaster.className.replace("show", "");
+        }, 3000)
     }
 
     function purchaseAutoClicker() {
-            if (cookies >= AutoClickerCost) {
-                cookies = cookies - AutoClickerCost;
-                AutoClickers++;
-                showToast("Autoclicker Purchased", "#333", "#000")
-            } else {
-              showToast("You cannot afford an autoclicker!", "rgb(121, 28, 28)")
-            }
-            setCookie("AutoClickers", AutoClickers, 30);
+        if (cookies >= AutoClickerCost) {
+            cookies = cookies - AutoClickerCost;
+            AutoClickers++;
+            showToast("Autoclicker Purchased", "#333", "#000")
+        } else {
+            showToast("You cannot afford an autoclicker!", "rgb(121, 28, 28)")
+        }
+        setCookie("AutoClickers", AutoClickers, 30);
     }
 
     function purchaseAutoClickerClicker() {
-            if (AutoClickers >= AutoClickerClickerCost) {
-                AutoClickers = AutoClickers - AutoClickerClickerCost;
-                AutoClickerClickers++;
-                showToast("Autoclicker Clicker Purchased", "#333", "#000")
-            } else {
-              showToast("You cannot afford an autoclicker clicker!", "rgb(121, 28, 28)")
-            }
-            setCookie("AutoClickerClickers", AutoClickerClickers, 30);
+        if (AutoClickers >= AutoClickerClickerCost) {
+            AutoClickers = AutoClickers - AutoClickerClickerCost;
+            AutoClickerClickers++;
+            showToast("Autoclicker Clicker Purchased", "#333", "#000")
+        } else {
+            showToast("You cannot afford an autoclicker clicker!", "rgb(121, 28, 28)")
+        }
+        setCookie("AutoClickerClickers", AutoClickerClickers, 30);
     }
 
     function upgradeLowerPricesAutoclickers() {
-            if (cookies >= PriceDecreaseCost) {
-                cookies = cookies - PriceDecreaseCost;
-                AutoClickerCost = (AutoClickerCost - (AutoClickerCost * 0.05)).toFixed(2);
-                CostReductionUpgradesAutoclicker++;
-                showToast("Decreased autoclicker price by 5%", "#333", "#000")
-            } else {
-              showToast("You cannot afford this upgrade!", "rgb(121, 28, 28)")
-            }
-            setCookie("CostReductionUpgradesAutoclicker", CostReductionUpgradesAutoclicker, 30);
+        if (cookies >= PriceDecreaseCost) {
+            cookies = cookies - PriceDecreaseCost;
+            AutoClickerCost = (AutoClickerCost - (AutoClickerCost * 0.05)).toFixed(2);
+            CostReductionUpgradesAutoclicker++;
+            showToast("Decreased autoclicker price by 5%", "#333", "#000")
+        } else {
+            showToast("You cannot afford this upgrade!", "rgb(121, 28, 28)")
+        }
+        setCookie("CostReductionUpgradesAutoclicker", CostReductionUpgradesAutoclicker, 30);
     }
 
     function upgradeLowerPricesAutoclickerClickers() {
-            if (cookies >= PriceDecreaseCost) {
-                cookies = cookies - PriceDecreaseCost;
-                AutoClickerClickerCost = (AutoClickerClickerCost - (AutoClickerClickerCost * 0.05)).toFixed(2);
-                CostReductionUpgradesAutoclickerClicker++;
-                showToast("Decreased autoclicker clicker price by 5%", "#333", "#000")
-            } else {
-              showToast("You cannot afford this upgrade!", "rgb(121, 28, 28)")
-            }
-            setCookie("CostReductionUpgradesAutoclickerClicker", CostReductionUpgradesAutoclickerClicker, 30);
+        if (cookies >= PriceDecreaseCost) {
+            cookies = cookies - PriceDecreaseCost;
+            AutoClickerClickerCost = (AutoClickerClickerCost - (AutoClickerClickerCost * 0.05)).toFixed(2);
+            CostReductionUpgradesAutoclickerClicker++;
+            showToast("Decreased autoclicker clicker price by 5%", "#333", "#000")
+        } else {
+            showToast("You cannot afford this upgrade!", "rgb(121, 28, 28)")
+        }
+        setCookie("CostReductionUpgradesAutoclickerClicker", CostReductionUpgradesAutoclickerClicker, 30);
     }
 
     function upgradeMoreCookiesPerClick() {
-            if (cookies >= ProductionIncreaseCost) {
-                cookies = cookies - ProductionIncreaseCost;
-                AutoclickersProductionRate++
-                showToast("Increased autoclicker production by 1", "#333", "#000")
-            } else {
-              showToast("You cannot afford this upgrade!", "rgb(121, 28, 28)")
-            }
-            setCookie("AutoClickersProductionRate", AutoClickersProductionRate, 30);
+        if (cookies >= ProductionIncreaseCost) {
+            cookies = cookies - ProductionIncreaseCost;
+            AutoclickersProductionRate++
+            showToast("Increased autoclicker production by 1", "#333", "#000")
+        } else {
+            showToast("You cannot afford this upgrade!", "rgb(121, 28, 28)")
+        }
+        setCookie("AutoClickersProductionRate", AutoClickersProductionRate, 30);
     }
 
     const everyTick = () => {
         if (AutoClickers > 0) {
-          let AutoclickersCookies = ((AutoClickers * 1) * AutoclickersProductionRate);
-          receiveCookieAuto(AutoclickersCookies);
+            let AutoclickersCookies = ((AutoClickers * 1) * AutoclickersProductionRate);
+            receiveCookieAuto(AutoclickersCookies);
         }
         if (AutoClickerClickers > 0) {
-          AutoClickers += (AutoClickerClickers * 1)
+            AutoClickers += (AutoClickerClickers * 1)
         }
         let formattedCookies = Math.round(cookies).toLocaleString()
         let formattedAutoClickers = AutoClickers.toLocaleString()
